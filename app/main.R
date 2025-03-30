c6b3381377f29b73df69a6ad806ccce6003c8287
Pierrick Kinif <pierrick-kinif@hotmail.be>
2025-03-30 10:31:24 +0200

    hello shiny example

--- app/main.R
+++ app/main.R
@@ -1,25 +1,32 @@
 box::use(
-  shiny[bootstrapPage, div, moduleServer, NS, renderUI, tags, uiOutput],
+  shiny[fluidPage, titlePanel, sidebarLayout, sidebarPanel, textInput, textOutput, mainPanel, NS, moduleServer,
+        renderText],
 )
 
 #' @export
 ui <- function(id) {
   ns <- NS(id)
-  bootstrapPage(
-    uiOutput(ns("message"))
+  
+  fluidPage(
+    titlePanel("Hello Rhino!"),
+    sidebarLayout(
+      sidebarPanel(
+        textInput(ns("name"), "Your name:", "World")
+      ),
+      mainPanel(
+        textOutput(ns("greeting"))
+      )
+    )
   )
 }
 
+
 #' @export
+
 server <- function(id) {
   moduleServer(id, function(input, output, session) {
-    output$message <- renderUI({
-      div(
-        style = "display: flex; justify-content: center; align-items: center; height: 100vh;",
-        tags$h1(
-          tags$a("Check out Rhino docs!", href = "https://appsilon.github.io/rhino/")
-        )
-      )
+    output$greeting <- renderText({
+      paste("Hello,", input$name, "!")
     })
   })
 }
